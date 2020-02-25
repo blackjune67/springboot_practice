935fbf421e23c57c0fa3b6d780ed321922f5e0c9
hajune <hajune67@smartix.co.kr>
2020-02-25 18:05:43 +0900

    spring commit

--- src/test/java/com/example/study/repository/UserRepositoryTest.java
+++ src/test/java/com/example/study/repository/UserRepositoryTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 public class UserRepositoryTest extends DemoApplicationTests {
 
@@ -15,19 +16,20 @@ public class UserRepositoryTest extends DemoApplicationTests {
     @Test
     public void create() {
         User user = new User();
-        user.setAccount("TestUser03");
-        user.setEmail("TestUser03@naver.com");
+        user.setAccount("TestUser01");
+        user.setEmail("TestUser01@naver.com");
         user.setPhoneNumber("010-1234-1013");
         user.setCreateAt(LocalDateTime.now());
-        user.setCreateBy("TestUser_Admin03");
+        user.setCreateBy("TestUser_Admin01");
 
         User newUser = userRepository.save(user);
-        System.out.println("new_User03 : " + newUser);
+        System.out.println("new_User01 : " + newUser);
     }
 
     @Test
-    public void reade() {
-
+    public void read() {
+        Optional<User> user = userRepository.findById(1L);
+        user.ifPresent(selectUser -> System.out.println(">>>>> user : " + user));
     }
 
     public void update() {
